1ef28ac27f248ec5091599cf40290ca40c03190b
Nathalia Irokawa <nathalia.iro.fernandes@gmail.com>
2024-07-18 20:23:34 -0300

    Adicionado funcionalidade para ativar e desativar música, e interatividade do botão de começar e pausar o timer

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ const subtitulo = document.querySelector('.app__title.app__title-strong');
 const botoes = document.querySelectorAll('.app__card-button');
 
 //Criando temporizadores
-const duracaoFoco = 1500;
-const duracaoDescansoCurto = 300;
-const duracaoDescansoLongo = 900;
+const startPauseBt = document.querySelector('#start-pause');
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
 
 //Trocando o fundo e as imagens
 focoBt.addEventListener('click', () => {
@@ -57,4 +58,55 @@ function alterarContexto(contexto) {
         default:
             break; //se não tiver nenhum dos cases ele vai dar break e não retornará nada
     }
+}
+
+//Adicionando música
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('./sons/luna-rise-part-one.mp3');
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+});
+
+//Timer
+const somEnd = new Audio('./sons/beep.mp3');
+const somStart = new Audio('./sons/play.wav');
+const somPausa = new Audio('./sons/pause.mp3');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const iniciarOuPausarIcon = document.querySelector('#start-pause img');
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0){
+        somStart.play();
+        zerar();
+        return
+    }
+    tempoDecorridoEmSegundos -= 1;
+    console.log(tempoDecorridoEmSegundos);
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar(){
+    if(intervaloId){
+        // somPausa.play();
+        zerar();
+        return
+    }
+    // somStart.play();
+    iniciarOuPausarBt.textContent = "Pausar";
+    iniciarOuPausarIcon.setAttribute('src', './imagens/pause.png');
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    iniciarOuPausarBt.textContent = "Começar";
+    iniciarOuPausarIcon.setAttribute('src', './imagens/play_arrow.png');
+    intervaloId = null;
 }
