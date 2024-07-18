cfabef1c52b136d3ec1ee9b4d2853cbefd3abb01
Nathalia Irokawa <nathalia.iro.fernandes@gmail.com>
2024-07-17 21:33:23 -0300

    Adicionado interação de mudança do banner e focus do botão ao escolher tempo

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
+//Escolhendo os tipos de descanso
 const html = document.querySelector('html');
+const botaoIniciar = document.querySelector('.app__card-primary-button');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const imgBanner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const subtitulo = document.querySelector('.app__title.app__title-strong');
+const botoes = document.querySelectorAll('.app__card-button');
 
+//Criando temporizadores
+const duracaoFoco = 1500;
+const duracaoDescansoCurto = 300;
+const duracaoDescansoLongo = 900;
+
+//Trocando o fundo e as imagens
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
+    alterarContexto('foco');
+    focoBt.classList.add('active');
 }) 
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
+    alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 }) 
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
+    alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
 }) 
+
+function alterarContexto(contexto) {
+    botoes.forEach((contexto) => {
+        contexto.classList.remove('active');
+    })
+
+    html.setAttribute('data-contexto', contexto);
+    imgBanner.setAttribute('src', `./imagens/${contexto}.png`);
+
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br /> 
+                <strong class="app__title-strong">mergulhe no que importa.</strong> `;
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada?<br />
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            ora de voltar à superfície.<br />
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+            break;
+        default:
+            break; //se não tiver nenhum dos cases ele vai dar break e não retornará nada
+    }
+}
